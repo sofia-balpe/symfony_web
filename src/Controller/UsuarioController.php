b4f9a0a887e60382a48a9bc1a30b994722887244
Sofia Baldissera Pereira <135541865+sofia-balpe@users.noreply.github.com>
2025-01-31 13:01:13 -0300

    symfonyWeb first

---
+++ src/Controller/UsuarioController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class UsuarioController extends AbstractController
+{
+    #[Route('/usuario', name: 'app_usuario')]
+    public function index(): Response
+    {
+        return $this->render('usuario/index.html.twig', [
+            'controller_name' => 'UsuarioController',
+        ]);
+    }
+}
