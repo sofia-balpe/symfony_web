96f8f15ebae306fd31e8ddda1bb5d1471a2d68a9
Sofia Baldissera Pereira <135541865+sofia-balpe@users.noreply.github.com>
2025-02-03 12:47:34 -0300

    crud tailwind

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -12,7 +13,22 @@ final class UsuarioController extends AbstractController
     public function index(): Response
     {
         return $this->render('usuario/index.html.twig', [
-            'controller_name' => 'UsuarioController',
+            'users' => [
+                ['id' => 1, 'Username'=>'sofia', 'Email' => 'sofia@gmail.com', 'fullname'=>'Sofi Baldissera' ],
+                ['id' => 2, 'Username'=>'sofia', 'Email' => 'sofia@gmail.com', 'fullname'=>'Sofi Baldissera']
+            ]
+
         ]);
+    }
+    #[Route('/create', name: 'app_usuario_create', methods:['GET'])]
+    public function create(){
+        return $this->render('usuario/create.html.twig');
+    }
+
+    #[Route('/usuario/create', name: 'app_usuario_register', methods:['POST'])]
+    public function recebeCreate(Request $request){
+        $post = $request->request->all();
+        dd($post);
+        return $this->render('usuario/create.html.twing');
     }
 }
